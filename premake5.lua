b5cddcdcb2e7f4fd0d0acf978e0da650dc6f38e6
DizzasTeR <dizzastermta@gmail.com>
2020-10-17 21:56:40 +0500

    LINUX: Add notes

--- premake5.lua
+++ premake5.lua
@@ -1,3 +1,13 @@
+--[[
+	INSTRUCTIONS TO BUILD & LINK ON LINUX (DEBIAN SPECIFICALLY)
+	1) dpkg --add-architecture i386
+	2) apt-get update
+	3) apt-get install build-essential gcc-multilib rpm libstdc++6:i386 libgcc1:i386 zlib1g:i386 libncurses5:i386 libmariadbclient-dev libmariadbclient-dev:i386
+	3.extra) If needed: apt install libmariadb libmariadb:i386
+	4) Generate gmake files with premake: premake gmake
+	5) make config=[release/release32]
+--]]
+
 workspace "VCMPLua"
 	architecture "x86_64"
 	startproject "LuaPlugin"
