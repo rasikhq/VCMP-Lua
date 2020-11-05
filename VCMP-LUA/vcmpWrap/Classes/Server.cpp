bab21b30a1c57b5f3d438e08289b8111e917171f
DizzasTeR <dizzastermta@gmail.com>
2020-11-05 18:10:20 +0500

    Added name and pass utils in Server class

--- VCMP-LUA/vcmpWrap/Classes/Server.cpp
+++ VCMP-LUA/vcmpWrap/Classes/Server.cpp
@@ -21,9 +21,13 @@ sol::object Server::getSettings() {
 	}
 	sol::table settings = Lua.create_table();
 
+	char PasswordBuffer[128];
+	g_Funcs->GetServerPassword(PasswordBuffer, sizeof(PasswordBuffer));
+
 	settings["maxPlayers"] = s_Settings.maxPlayers;
 	settings["port"] = s_Settings.port;
 	settings["serverName"] = std::string(s_Settings.serverName);
+	settings["serverPassword"] = std::string(PasswordBuffer);
 	settings["flags"] = s_Settings.flags;
 
 	return settings;
@@ -31,7 +35,17 @@ sol::object Server::getSettings() {
 
 void Server::Init(sol::state* L) {
 	sol::state& state = *L;
-	L->set("Server", L->create_table_with("getSettings", &Server::getSettings));
-	state["Server"]["getOption"] = &Server::getOption;
-	state["Server"]["setOption"] = &Server::setOption;
+	sol::table ServerTable = L->create_table_with("getSettings", &Server::getSettings);
+	ServerTable["getOption"] = &Server::getOption;
+	ServerTable["setOption"] = &Server::setOption;
+
+	ServerTable.set_function("setName", [](const std::string& newName) {
+		g_Funcs->SetServerName(newName.c_str());
+	});
+
+	ServerTable.set_function("setPassword", [](const std::string& newName) {
+		g_Funcs->SetServerPassword(newName.c_str());
+	});
+
+	state["Server"] = ServerTable;
 }
